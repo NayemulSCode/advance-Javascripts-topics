fc451d9c8f4c8f6ea9b97d705d3e4376fd73a84e
Nayemul <nayemulhasan2016@gmail.com>
2021-02-04 02:13:54 +0600

    object Property by arrow function

--- objectProperty.js
+++ objectProperty.js
@@ -1,6 +1,22 @@
 // students নামে একটা অ্যারে থাকবে(id and name), একটা ফর লুপ ব্যবহার করে অ্যারের প্রতেকটি অবজেক্টকে ধরতে হবে
 // এবং সেখান থেকে শুধু name (নাম) গুলোকে নিয়ে অন্য একটা অ্যারেতে রাখতে হবে 
 // এবং id গুলোকে আলাদা অ্যারেতে রাখতে হবে; 
+
+// const names = [];
+// const ids = [];
+// for(let i=0; i<students.length; i++){
+//     const element = students[i]
+//     const name = element.name;
+//     const id = element.id;
+//     names.push(name);
+//     ids.push(id);
+//     console.log(names);
+//     console.log(ids);
+// }
+
+//এই কাজটি করা যায় ম্যাপ ফাংশন ব্যবহার করে 
+// map() এর কাজ হলো নিজে একটা অ্যারে বানিয়ে তার মধ্য নামে গুলো রেখে দিবে , 
+//আলাদা করে  const names = []; এইভাবে খালি অ্যারে declare করতে হবে না! 
 const students = [
     {id: 11, name: 'Nayemul Hasan'},
     {id: 15, name: 'Saheb Hasan'},
@@ -8,14 +24,7 @@ const students = [
     {id: 21, name: 'Faysal Ahamed'},
     {id: 14, name: 'Asad Ullah'}
 ];
-const names = [];
-const ids = [];
-for(let i=0; i<students.length; i++){
-    const element = students[i]
-    const name = element.name;
-    const id = element.id;
-    names.push(name);
-    ids.push(id);
-    console.log(names);
-    console.log(ids);
-}
+const names = students.map( s => s.name);
+const ids = students.map( x => x.id);
+console.log(names);
+console.log(ids);
