f9642a9ed8bb35160120de638cc7b9778be9571f
Nayemul <nayemulhasan2016@gmail.com>
2021-02-04 00:03:42 +0600

    squre series by array map() function and arrow function =>

--- map_filter_find.js
+++ map_filter_find.js
@@ -1,14 +1,4 @@
 const numbers = [2,3,4,5,6,7,8];
-const output = [];
 
-// for(let i=0 ; i<numbers.length; i++){
-//     const element = numbers[i];
-//     const result = element * element;
-//     output.push(result);
-// }
-// console.log(output);
-// map function এর তিনটি প্যারামিটার থাকে ১। এলিমেন্ট(element) 2। ইন্ডেক্স(index) 3.অ্যারে(array)
-const result =  numbers.map(function(element){
-    return element * element;
-})
+const result = numbers.map(x => x * x); //same as const squre = element => element * element; 
 console.log(result);
