5c5cfee6951fda2fae824397330e55669303963a
Nayemul <nayemulhasan2016@gmail.com>
2021-02-04 00:09:02 +0600

    filter on array

--- map_filter_find.js
+++ map_filter_find.js
@@ -1,4 +1,5 @@
 const numbers = [2,3,4,5,6,7,8];
-
-const result = numbers.map(x => x * x); //same as const squre = element => element * element; 
-console.log(result);
+//filter মানে হলো কন্ডিশনের মধ্য কে কে পরে যেমন এখানে বলা হইছে numbers array মধ্যে ৫ থেকে বড় কে কে আছে 
+//  x => x>5
+const larger = numbers.filter(x => x >5) 
+console.log(larger);
