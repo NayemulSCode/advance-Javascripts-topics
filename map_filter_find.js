3dca21f54cd62257497166e7f55e43efc122e7a5
Nayemul <nayemulhasan2016@gmail.com>
2021-02-04 00:17:27 +0600

    find on array

--- map_filter_find.js
+++ map_filter_find.js
@@ -1,5 +1,8 @@
 const numbers = [2,3,4,5,6,7,8];
 //filter মানে হলো কন্ডিশনের মধ্য কে কে পরে যেমন এখানে বলা হইছে numbers array মধ্যে ৫ থেকে বড় কে কে আছে 
 //  x => x>5
-const larger = numbers.filter(x => x >5) 
+const larger = numbers.filter(x => x >5);
+//find মানে হলো এই numbers অ্যারে মধ্যে এই x>5  কন্ডিশন এর মধ্যে কোন এলিমেন্ট থাকলে তাদের মধ্যে প্রথম এলিমেন্ট কে দেখাবে বা বের করে দিবে 
+const isThere  = numbers.find(x => x >5);
+console.log(isThere);
 console.log(larger);
