7b20146da9bfd86780ea875c834889f006c68a57
Nayemul <nayemulhasan2016@gmail.com>
2021-02-03 19:54:21 +0600

    == vs ===

---
+++ double == vs triple ===.js
@@ -0,0 +1,10 @@
+const first =0;
+const second = false;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+/// == mean , check the value between two variable
+//=== means , check the data_type and variable value between variable
--- nullVsUndefine.js
+++ nullVsUndefine.js
@@ -1,3 +1,8 @@
-//if you declare a variable but not assign value
-const nayemul;
-console.log(nayemul);
+//1.if you declare a variable but not assign value
+// const nayemul;
+// console.log(nayemul);
+//1.if you declare a function but not return value but you call this function
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+const result = add(23,45);
