28754252e443f9393fb5ac7079a94d417a1d9b35
Nayemul <nayemulhasan2016@gmail.com>
2021-02-03 23:53:21 +0600

    map function parameters

--- map_filter_find.js
+++ map_filter_find.js
@@ -1,9 +1,13 @@
 const numbers = [2,3,4,5,6,7,8];
 const output = [];
 
-for(let i=0 ; i<numbers.length; i++){
-    const element = numbers[i];
-    const result = element * element;
-    output.push(result);
-}
-console.log(output);
+// for(let i=0 ; i<numbers.length; i++){
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+// console.log(output);
+// map function এর তিনটি প্যারামিটার থাকে ১। এলিমেন্ট(element) 2। ইন্ডেক্স(index) 3.অ্যারে(array)
+numbers.map(function(element, index, array){
+    console.log(element, index, array);
+})
