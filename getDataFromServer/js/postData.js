ae8750f165d73ae7a982af99f6bb0c60bde800b4
Nayemul <nayemulhasan2016@gmail.com>
2021-02-05 11:30:36 +0600

    post data from browser

--- getDataFromServer/js/postData.js
+++ getDataFromServer/js/postData.js
@@ -1,17 +1,26 @@
 
-const postInfo= {
-    title: 'foo',
-    body: 'bar',
-    userId: 1,
-  }
+// const postInfo= {
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//   }
 
-
-fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify(postInfo),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
+document.getElementById('postbtn').addEventListener('click',function(){
+    const title = document.getElementById('title').value;
+    const bodyContent = document.getElementById('postbody').value;
+    const post = {title:title, body:bodyContent};
+    postServer(post);
+    console.log(title, bodyContent);
 })
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+//server এ ডাটা পোস্ট করতে গেলে মেথড, বডি, আর হেডার লাগবে! ফেচ এর মাধ্যমে সার্ভার ডাটা পোস্ট করি! 
+function postServer(postInfo){
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(postInfo),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+}
