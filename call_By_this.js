d248233d4dcc2a9d800aee0ec32dd5c9f9e381d6
Nayemul <nayemulhasan2016@gmail.com>
2021-02-05 17:23:50 +0600

    call by this

---
+++ call_By_this.js
@@ -0,0 +1,18 @@
+// একটি ম্যাথড এর মধ্য তার প্রোপার্টি এক্সেস করতে চাইলে আমাদের this ব্যবহার করতে হবে , this.firstName
+// আর ম্যাথডের প্রোপার্টি বাইরে থেকে এক্সেস করতে হলে, যে মেথডের প্রোপার্টি এক্সেস করবো সেই 
+// method এর নাম dot দিয়ে প্রোপার্টি এর নাম যেমনঃ Person.salary
+const Person ={
+    firstName : "nayemul",
+    lastName :"Hasan",
+    salary : 25000,
+    getFullName: function(){
+        console.log(this.firstName, this.lastName);
+    },
+    bill: function(amount){
+        this.salary = this.salary - amount;
+        return this.salary;
+    }
+}
+console.log(Person.firstName);
+Person.bill(1550);
+console.log(Person.salary);
